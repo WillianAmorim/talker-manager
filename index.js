70d6b216b9865adc59b523cfb873413983f8f84f
Willian <willian.amorim1@outlook.com>
2022-06-26 19:52:19 -0300

    feito rota /talker - primeiro requisito

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs/promises');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -6,6 +7,13 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', async (req, res) => {
+  const talkers = await fs.readFile('talker.json');
+  if (!talkers) res.status(HTTP_OK_STATUS).json([]);
+
+  res.status(HTTP_OK_STATUS).json(JSON.parse(talkers));
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
