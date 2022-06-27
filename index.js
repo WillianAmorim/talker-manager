a430a530b9978673097ea96383d60a594a2b750c
Willian <willian.amorim1@outlook.com>
2022-06-27 00:00:55 -0300

    feito rota getId - requisito 2

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const FILE_JSON = 'talker.json';
 
-app.get('/talker', async (req, res) => {
+app.get('/talker', async (_req, res) => {
   const talkers = await fs.readFile('talker.json');
   if (!talkers) res.status(HTTP_OK_STATUS).json([]);
 
   res.status(HTTP_OK_STATUS).json(JSON.parse(talkers));
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const obj = await fs.readFile(FILE_JSON);
+  const talkers = JSON.parse(obj);
+
+  const talkerId = talkers.find((talker) => talker.id === Number(id));
+  if (!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  return res.status(HTTP_OK_STATUS).json(talkerId);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
