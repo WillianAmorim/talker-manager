9d247365b01a7df141586a178ec3e25c50a8d2d2
Willian <willian.amorim1@outlook.com>
2022-06-27 17:02:45 -0300

    utilizando os middlewares de validacao e o gerador de token no index

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs/promises');
 const express = require('express');
 const bodyParser = require('body-parser');
+const { emailValidate, passwordValidade } = require('./middlewares/validations');
+const tokenGenerate = require('./middlewares/tokenGenerate');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,6 +11,7 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 const FILE_JSON = 'talker.json';
 
+// REQUISITO 01
 app.get('/talker', async (_req, res) => {
   const talkers = await fs.readFile('talker.json');
   if (!talkers) res.status(HTTP_OK_STATUS).json([]);
@@ -16,6 +19,7 @@ app.get('/talker', async (_req, res) => {
   res.status(HTTP_OK_STATUS).json(JSON.parse(talkers));
 });
 
+// REQUSITO 02
 app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
   const obj = await fs.readFile(FILE_JSON);
@@ -25,6 +29,12 @@ app.get('/talker/:id', async (req, res) => {
   if (!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
   return res.status(HTTP_OK_STATUS).json(talkerId);
+});
+
+// REQUISITO 03
+app.post('/login', emailValidate, passwordValidade, (req, res) => {
+  const token = tokenGenerate();
+  res.status(200).json({ token });
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
